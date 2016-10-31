6d58d29d38e6517d2a45972ad241e08682ce1e16
Nounours <le-run@crans.org>
2016-10-31 15:02:41 +0100

    Added some c functions to the kernel ebryo

--- kernel.c
+++ kernel.c
@@ -3,15 +3,50 @@
 #include <stdint.h>
 
 #define VIDEO 0xB8000
+#define V_W 80
+#define V_H 25
+
+void memcpy(void * dest, void * src, int32_t length)
+{
+    uint8_t * d = (uint8_t*)dest;
+    uint8_t * s = (uint8_t*)src;
+    for(uint32_t i = 0; i < length; i++)
+        d[i] = s[i];
+}
+
+uint8_t cursor[2] = { 0, 0 };
+
+void newline()
+{
+    void * src = (void*)(VIDEO + 2 * V_W);
+    memcpy((void*)VIDEO, src, 2 * V_W * (V_H - 1));
+}
+
+void move(uint8_t x, uint8_t y)
+{
+    if(x >= V_W)
+    {
+        y += x / V_W;
+        x %= V_W;
+    }
+    if(y >= V_H)
+    {
+        for(; y > V_H; y--)
+            newline();
+        y = V_H;
+    }
+    cursor[0] = x;
+    cursor[1] = y;
+}
 
 void putc(char c)
 {
-    char * video = (char*)VIDEO;
+    char * video = (char*)VIDEO + 2 * (V_W * cursor[1] + cursor[0]);
     video[0] = c;
     video[1] = 0x07;
 }
 
 void kernel_main(void)
 {
-    putc('>');
+    memcpy((void*)(VIDEO + 2 * V_W), (void*)VIDEO, 2 * V_W);
 }
